9b37e5e9cba891ef9cb2145507ae1f6a64aee2b8
Volodya Kushch <110915611+Volodya460@users.noreply.github.com>
2024-04-05 12:36:24 +0300

    fix

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -26,7 +26,6 @@ export default function ContactForm({ hendleSubmit }) {
           <FieldForm
             type="text"
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
@@ -36,7 +35,6 @@ export default function ContactForm({ hendleSubmit }) {
           <FieldForm
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
